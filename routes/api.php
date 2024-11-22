4885714b2a957bc4ae2a7309a7dabcad1cb9aa69
JoseAntonio <joseantoniogimenez041@gmail.com>
2024-11-22 19:06:04 +0100

    Jose Antonio Gimenez Martinez

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
-use App\Http\Controllers\JsonController;
 
-Route::get('/json', [JsonController::class, 'index']);
-Route::post('/json', [JsonController::class, 'store']);
-Route::get('/json/{filename}', [JsonController::class, 'show']);
-Route::put('/json/{filename}', [JsonController::class, 'update']);
-Route::delete('/json/{filename}', [JsonController::class, 'destroy']);
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JsonController;
+use App\Http\Controllers\HelloWorldController;
+
+// Rutas para el JsonController
+Route::prefix('json')->group(function () {
+    Route::get('/', [JsonController::class, 'index']); // Listar los archivos JSON
+    Route::post('/', [JsonController::class, 'store']); // Guardar un archivo JSON
+    Route::get('/{filename}', [JsonController::class, 'show']); // Ver el contenido de un archivo JSON
+    Route::put('/{filename}', [JsonController::class, 'update']); // Actualizar un archivo JSON
+    Route::delete('/{filename}', [JsonController::class, 'destroy']); // Eliminar un archivo JSON
+});
+
+// Rutas para el HelloWorldController
+Route::apiResource('hello', HelloWorldController::class); // CRUD completo para el controlador HelloWorldController
+
